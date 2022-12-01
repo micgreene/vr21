7add476c7a0b9c0ed6406c3ea20cc2a0026d2f4f
Michael Greene <mgreene11111@yahoo.com>
2022-12-01 12:02:49 -0800

    node.js and JavaScript practice

---
+++ JS/function-practice.js
@@ -0,0 +1,241 @@
+const prompt = require('prompt');
+
+function difference(a, b) {
+    return a - b;
+}
+
+console.log(difference(2, 2));
+console.log(difference(0, 2));
+
+function product(a, b) {
+    return a * b;
+}
+
+console.log(product(2, 2)); // 4
+console.log(product(0, 2)); // 0
+
+function printDay(day) {
+    switch (day) {
+        case 1: {
+            return "Sunday";
+            break;
+        }
+        case 2: {
+            return "Monday";
+            break;
+        }
+        case 3: {
+            return "Truesday";
+            break;
+        }
+        case 4: {
+            return "Wednesday";
+            break;
+        }
+        case 5: {
+            return "Thrusday";
+            break;
+        }
+        case 6: {
+            return "Friday"
+            break;
+        }
+        case 7: {
+            return "Saturday"
+            break;
+        }
+        default: {
+            return undefined;
+        }
+    }
+}
+
+console.log(printDay(4)); // "Wednesday"
+console.log(printDay(41)); // undefined
+
+function lastElement(arr) {
+    return arr[arr.length - 1]
+}
+
+console.log(lastElement([1, 2, 3, 4])); // 4
+console.log(lastElement([])); // undefined
+
+
+function numberCompare(a, b) {
+    if (a > b) {
+        return "First is greater";
+    } else if (a < b) {
+        return "Second is greater";
+    } else {
+        return "Numbers are equal";
+    }
+}
+
+console.log(numberCompare(1, 1)); // "Numbers are equal"
+console.log(numberCompare(2, 1)); // "First is greater"
+console.log(numberCompare(1, 2)); // "Second is greater"
+
+function singleLetterCount(str, char) {
+    str = str.toLowerCase();
+    let returnVal = 0;
+
+    for (let ch of str) {
+        if (ch == char) returnVal += 1;
+    }
+
+    return returnVal;
+}
+
+console.log(singleLetterCount('amazing', 'A')); // 2
+console.log(singleLetterCount('Rithm School', 'o')); // 2
+
+function multipleLetterCount(word) {
+    retObj = {};
+
+    for (let ch of word) {
+        if (ch in retObj) retObj[ch] += 1;
+        else retObj[ch] = 1;
+    }
+
+    return retObj
+}
+
+console.log(multipleLetterCount("hello")); // {h:1, e: 1, l: 2, o:1}
+console.log(multipleLetterCount("person")); // {p:1, e: 1, r: 1, s:1, o:1, n:1}
+
+function arrayManipulation(arr, cmd, loc, val) {
+    cmd = cmd.toLowerCase();
+    loc = loc.toLowerCase();
+
+    switch (arguments.length) {
+        case arguments.length < 3: {
+            return "Too few arguments! You should supply an ARRAY, COMMAND, and LOCATION at a minimum.";
+        }
+        case 3: {
+            if (cmd == "remove") {
+                if (loc == "beginning") {
+                    return arr.shift();
+                } else if (loc == "end") {
+                    return arr.pop();
+                }
+            }
+            break;
+        }
+        case 4: {
+            if(cmd == "add"){
+                if (loc == "beginning") {
+                    arr.unshift(val);
+                    return arr;
+                } else if (loc == "end") {
+                    arr.push(val);
+                    return arr;
+                }
+            }
+            break;
+        }
+    }
+}
+
+console.log(arrayManipulation([1, 2, 3], "remove", "end")); // 3
+console.log(arrayManipulation([1, 2, 3], "remove", "beginning")); // 1
+console.log(arrayManipulation([1, 2, 3], "add", "beginning", 20)); // [20,1,2,3]
+console.log(arrayManipulation([1, 2, 3], "add", "end", 30)); // [1,2,3,30]
+
+function isPalindrome(str) {
+    var j = str.length - 1;
+
+    for (var i = 0; i < str.length; i++) {
+        if (j == i || i > j && j < i) 
+            return true;
+        else if (str[i] == str[j]){
+            j--;
+            continue;
+        } 
+        else 
+            return false;
+    } 
+}
+
+console.log(isPalindrome('testing')); // false
+console.log(isPalindrome('tacocat')); // true
+console.log(isPalindrome('hannah')); // true
+console.log(isPalindrome('robert')); // false
+
+function rockPaperScissors() {
+    console.log("\n\nWelcome to Rock-Paper-Scissors!\n");
+    console.log(("Select a Sign to Throw! (Rock, Paper, or Scissors)"));
+
+    let cpuChoice = Math.floor(Math.random() * 3) + 1;
+    let signToBeat = "";
+
+    let rockWin = "Rock breaks Scissors.";
+    let paperWin = "Paper covers Rock.";
+    let scissorsWin = "Scissors cut Paper.";
+    let win = "You Win!\n";
+    let lose = "You Lose!\n";
+    let tie = "You chose the same sign. It's a Tie!\n"
+
+    prompt.start();
+
+    prompt.get(['choice'], function (err, result) {
+        if (err) {
+          return onErr(err);
+        }      
+
+        result.choice = result.choice.toLowerCase();
+
+        switch(cpuChoice) {
+            case 1: {
+                signToBeat = "Rock";
+                break;
+            }
+            case 2: {
+                signToBeat = "Paper";
+                break;
+            }
+            case 3: {
+                signToBeat = "Scissors"
+            }
+        }
+        console.log("\nCPU has selected: " + signToBeat + "!");
+
+        switch(result.choice) {            
+            case "rock": {
+                if(signToBeat == "Paper") {
+                    console.log(paperWin + lose);
+                } else if(signToBeat == "Scissors") {
+                    console.log(rockWin + win);
+                } else if(signToBeat == "Rock") {
+                    console.log(tie);
+                }
+                break;
+            }
+            case "paper": {
+                if(signToBeat == "Paper") {
+                    console.log(tie);
+                } else if(signToBeat == "Scissors") {
+                    console.log(scissorsWin + lose);
+                } else if(signToBeat == "Rock") {
+                    console.log(paperWin + win);
+                }
+                break;
+            }
+            case "scissors": {
+                if(signToBeat == "Paper") {
+                    console.log(scissorsWin + win);
+                } else if(signToBeat == "Scissors") {
+                    console.log(tie);
+                } else if(signToBeat == "Rock") {
+                    console.log(rockWin + lose);
+                }
+                break;
+            }
+            default: {
+                console.log("You must select ROCK, PAPER, or SCISSORS to play!");
+            }
+        }    
+    });    
+}
+
+rockPaperScissors();
+
--- README.md
+++ README.md
@@ -1,1 +1,133 @@
-# vr21
+# JavaScript Review
+### Notes from Eloquent JavaScript 3rd Ed. - [Link](https://eloquentjavascript.net)
+
+## Values, Types, and Operators
+### Special Numbers
+- Infinity
+- -Infinity
+- NaN
+
+### Unary Operators
+- Some operators are not symbols [e.g. &, *, |, +, %]
+- `typeof` for example
+
+### Empty Values
+- undefined <-- doesn't exist yet
+- null <-- instantiated, but holds no value
+
+---
+
+## Program Structure
+### Expressions
+- A fragment of code that produces a value is called an expression. 
+- Every value that is written literally (22 or "psychoanalysis") is an expression.
+
+### Statements
+- A computer program is a list of "instructions" to be "executed" by a computer.
+- In a programming language, these programming instructions are called **statements**.
+- *JavaScript statements are composed of:*
+    - **Values, Operators, Expressions, Keywords, and Comments**
+
+### Bindings
+- To catch and hold values, JavaScript provides a thing called a **binding**, or variable:
+    - `let caught = 5 * 5;`
+- A single let statement may define multiple bindings. The definitions must be separated by commas.
+    - `let one = 1, two = 2;`
+- **Binding names:**
+    - must not start with a digit
+    - may include dollar signs ($) or underscores (_)
+    - no other punctuation or special characters.
+
+### The Environment
+- The collection of bindings and their values that exist at a given time is called the environment.
+- When a program starts up, this environment is not empty. It always contains bindings that are part of the language standard.
+- Has bindings that provide ways to interact with the surrounding system.
+
+---
+
+## JavaScript and the Browser
+### Networks and the Internet
+- A network protocol describes a style of communication over a network.
+- **Hypertext Transfer Protocol (HTTP)**
+    - protocol for retrieving named resources (chunks of information, such as web pages or pictures). 
+    - It specifies that the side making the request should start with a line like this, naming the resource and the version of the protocol that it is trying to use:
+        - `GET /index.html HTTP/1.1`
+
+- **The Transmission Control Protocol (TCP)** 
+    - All Internet-connected devices “speak” it, and most communication on the Internet is built on top of it.
+    - one computer must be waiting, or listening, for other computers to start talking to it. To be able to listen for different kinds of communication at the same time on a single machine, each listener has a number (called a port) associated with it. 
+        - *Most protocols specify which port should be used by default.* 
+    - Another computer can then establish a connection by connecting to the target machine using the correct port number. 
+    - If the target machine can be reached and is listening on that port, the connection is successfully created. 
+        - *The listening computer is called the **server**, and the connecting computer is called the **client**.*8*
+
+### The Web
+- The World Wide Web (not to be confused with the Internet as a whole) is a set of protocols and formats that allow us to visit web pages in a browser. 
+- To become part of the Web, all you need to do is connect a machine to the Internet and have it listen on port 80 with the HTTP protocol so that other computers can ask it for documents.
+- Each document on the Web is named by a Uniform Resource Locator (URL), which looks something like this:
+  http://eloquentjavascript.net/13_browser.html
+ |      |                      |               |
+ protocol       server               path
+ 
+---
+
+## The Document Object Model
+### Document structure
+- You can imagine an HTML document as a nested set of boxes. 
+- Tags such as <body> and </body> enclose other tags, which in turn contain other tags or text. Here’s the example document from the previous chapter:
+<!doctype html>
+<html>
+  <head>
+    <title>My home page</title>
+  </head>
+  <body>
+    <h1>My home page</h1>
+    <p>Hello, I am Marijn and this is my home page.</p>
+    <p>I also wrote a book! Read it
+      <a href="http://eloquentjavascript.net">here</a>.</p>
+  </body>
+</html>
+
+### Trees
+- We call a data structure a tree when it has a branching structure, has no cycles (a node may not contain itself, directly or indirectly), and has a single, well-defined root. 
+- In the case of the DOM, document.documentElement serves as the root.
+![Visualization of Document Tree:](https://eloquentjavascript.net/img/html-tree.svg)
+
+### Finding elements
+- If we want to find a specific node in the document, reaching it by starting at document.body and following a fixed path of properties is a bad idea.
+- It’d be better if we could say “Get the first link in the document”. And we can.
+    - `let link = document.body.getElementsByTagName("a")[0];`
+- To find a specific single node, you can give it an id attribute and use document.getElementById instead:
+    - `<img id="gertrude" src="img/ostrich.png"></p>`
+    - `let ostrich = document.getElementById("gertrude");`
+
+### Changing the Document
+- Almost everything about the DOM data structure can be changed.
+- To add a child node to an element node:
+    -  *appendChild* puts it at the end of the list of children
+    - *insertBefore* inserts the node given as the first argument before the node given as the second argument.
+        - `let paragraphs = document.body.getElementsByTagName("p");`
+        - `document.body.insertBefore(paragraphs[2], paragraphs[0]);`
+
+### Creating nodes
+- write a script that replaces all images (<img> tags) in the document with the text held in their alt attributes, which specifies an alternative textual representation of the image.
+- This involves not only removing the images but adding a new text node to replace them. 
+- Text nodes are created with the document.createTextNode method:
+<p>The <img src="img/cat.png" alt="Cat"> in the
+  <img src="img/hat.png" alt="Hat">.</p>
+
+<p><button onclick="replaceImages()">Replace</button></p>
+
+<script>
+  function replaceImages() {
+    let images = document.body.getElementsByTagName("img");
+    for (let i = images.length - 1; i >= 0; i--) {
+      let image = images[i];
+      if (image.alt) {
+        let text = document.createTextNode(image.alt);
+        image.parentNode.replaceChild(text, image);
+      }
+    }
+  }
+</script>
+

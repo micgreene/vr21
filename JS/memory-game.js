fa6ef1c01a1b4a6e535aa75e9ccc606fec0ce9b7
Michael Greene <mgreene11111@yahoo.com>
2022-12-07 13:41:07 -0800

    finished basic logic for memory game

---
+++ JS/memory-game.js
@@ -0,0 +1,40 @@
+'use strict';
+let cardsFaceUp = 0;
+let card1;
+let card2;
+const cards = document.querySelectorAll('.flip-card-inner');
+
+[...cards].forEach((card)=>{
+  card.addEventListener( "click", () => {
+    if(!card.classList.contains("is-flipped")) {
+        // console.log("Card1: " + card.lastElementChild.innerText);
+        cardsFaceUp++;
+        if(cardsFaceUp === 1) {
+            card1 = card;
+            card.classList.toggle('is-flipped');
+        }
+        else if(cardsFaceUp === 2) {
+            card2 = card;
+            card.classList.toggle('is-flipped');
+            checkMatch(card1, card2);            
+        } 
+               
+    }
+
+    if(cardsFaceUp == 2) {
+
+    }
+  });
+});
+
+function checkMatch(card1, card2) {
+    if(card1.lastElementChild.innerText === card2.lastElementChild.innerText) {
+        cardsFaceUp = 0;
+    } else {
+        setTimeout(() => {
+            cardsFaceUp = 0;
+            card1.classList.toggle('is-flipped');
+            card2.classList.toggle('is-flipped');
+        }, 1500);
+    }
+}
